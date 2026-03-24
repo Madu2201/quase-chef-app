fc14f1794093708365e133798c49ec883012f0cb
Maria Eduarda de Almeida <dudacsantos450@gmail.com>
2026-03-24 09:48:42 -0300

    Update receitas.tsx

--- app/(tabs)/receitas.tsx
+++ app/(tabs)/receitas.tsx
@@ -1,37 +1,9 @@
-import { View, Text, FlatList } from "react-native";
-import Header from "../../components/header";
+import { View, Text } from 'react-native';
 
-const receitas = [
-  { id: "1", nome: "Arroz com legumes" },
-  { id: "2", nome: "Macarrão ao alho e óleo" },
-  { id: "3", nome: "Salada tropical" },
-  { id: "4", nome: "Sopa de abóbora" },
-];
-
-export default function ReceitasScreen() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "#FFF3E8" }}>
-      <Header title="Receitas" />
-
-      <FlatList
-        contentContainerStyle={{ padding: 20 }}
-        data={receitas}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View
-            style={{
-              padding: 12,
-              marginBottom: 8,
-              backgroundColor: "#ffffff",
-              borderRadius: 8,
-              borderWidth: 1,
-              borderColor: "#94A3B8",
-            }}
-          >
-            <Text style={{ fontSize: 18, color: "#475569" }}>{item.nome}</Text>
-          </View>
-        )}
-      />
-    </View>
-  );
+export default function TabScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Em breve...</Text>
+        </View>
+    );
 }
